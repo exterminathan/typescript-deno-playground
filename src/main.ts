8d1e18bf29eb6f04452613235e85770059963366
Nathan Shturm <exterminathan@gmail.com>
2024-11-14 12:39:13 -0800

    Initial Files
    Added all files necessary to startup a Deno/Typescript environment
    Time to see if this works for github pages

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// ~------------------INTRO SETUP----------------~
+
+// @deno-types="npm:@types/leaflet@^1.9.14"
+// leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+import "./leafletWorkaround.ts";
+//import luck from "./luck.ts";
+
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+/// <reference lib="deno.ns" />
+
+export default {
+    base: Deno.env.get("REPO_NAME") || "/project",
+  };
+  
