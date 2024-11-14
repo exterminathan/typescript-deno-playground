edfa19b145a6d072c5ce57f17b9ce34977c9e542
Nathan Shturm <exterminathan@gmail.com>
2024-11-14 12:57:06 -0800

    slight changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 // ~------------------INTRO SETUP----------------~
 
 // @deno-types="npm:@types/leaflet@^1.9.14"
-// leaflet from "leaflet";
+import leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./style.css";
 import "./leafletWorkaround.ts";
 //import luck from "./luck.ts";
 
+const map = leaflet.map("map", {
+  center: leaflet.latLng(0,0),
+  zoom: 20,
+  minZoom: 20,
+  maxZoom: 20,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+
+console.log(map);
